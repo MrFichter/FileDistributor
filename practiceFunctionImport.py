b56f55a652b74669831802110af60bfa745308ce
Jonathan Fichter <jonathan.fichter@live.com>
2012-08-04 13:24:55 -0400

    Learned how to call a function from another file.
    Next steps:
    1) Call splitString () from fileDistributor.py.
    2) Use splitString () as a way to fix the problem where it creates
    fractionsProject.sbFichterJ rather than fractionsProjectFichterJ.sb
    3) Find the ### changes you made for testing purposes and undo them.
    
    Learning note: I deleted a line. GitHub highlighted it in red and put a
    minus sign before it. I added two lines. GitHub highlighted them in blue
    and put a plus sign before each one.

--- practiceFunctionImport.py
+++ practiceFunctionImport.py
@@ -1,4 +1,5 @@
-import splitString.py
+from splitString import splitString 
+# From (filename with .py chopped off) import (function, in this case.)
 
 fileName, fileType = splitString ('fractions.sb' , '.')
 print 'fileName is ' , fileName
