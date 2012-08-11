1f8a34cfd6b7c18b7c2a831bbf7e1a86c81ddf72
Jonathan Fichter <jonathan.fichter@live.com>
2012-08-11 11:17:28 -0400

    simplified splitString()
    Used a cleaner loop thanks to the range() function. Also deleted the
    sample text file I added via a recent practice commit.

--- splitString.py
+++ splitString.py
@@ -1,17 +1,16 @@
 def splitString (origString, splitPoint):
-    '''Splits origString into beforeSplit and afterSplit. The splitPoint is a character somewhere within origString. Example: splitPoint might be a period separating the file name from the file type.'''
+    '''Splits origString into beforeSplit and afterSplit. \
+The splitPoint is a character somewhere within origString. \
+Example: splitPoint might be a period separating the file \
+name from the file type. NB: If there are two periods in \
+origString, the program treats the first as the splitPoint.'''
 
     #Find location of splitPoint.
-    i = 0
-    for character in origString:
-        if character == '.':
-            splitLocation = i
-        else: i += 1
-
-    #Assign characters before splitPoint to string called beforeSplit.
-    beforeSplit = origString [0 : splitLocation]
-
-    #Assign characters after splitPoint to string called afterSplit.
-    afterSplit = origString [ (splitLocation + 1) : ] # I added +1 to splitLocation because I do not want to include the splitLocation character.
-               
-    return (beforeSplit, afterSplit)
+    for i in range (0, len(origString) ):
+        if origString[i] == '.':
+            splitLocation = i 
+            #Assign characters before splitPoint to string called beforeSplit.
+            beforeSplit = origString [0 : splitLocation]
+            #Assign characters after splitPoint to string called afterSplit.
+            afterSplit = origString [ (splitLocation + 1) : ] # I added +1 to splitLocation because I do not want to include the splitLocation character.
+            return (beforeSplit, afterSplit)
