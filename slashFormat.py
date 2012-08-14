3d38a1fbf19cb10ef30351f66d280adcecfac6cc
Jonathan Fichter <jonathan.fichter@live.com>
2012-08-13 20:18:54 -0400

    debugged slashFormat fucntion

--- slashFormat.py
+++ slashFormat.py
@@ -1,6 +1,9 @@
 def slashFormat (rawUserInput):
-    '''Converts a string's backslashes to forward slashes and makes sure to add a forward slash at the end.'''
+    '''Converts a string's backslashes to forward slashes and \
+makes sure to add a forward slash at the end.'''
     pathConvSlash = rawUserInput.replace('\\' , '/') ##Convert backslashes to forward slashes.
     if pathConvSlash [len(pathConvSlash) - 1] != '/': ## If the final character is not a forward slash...
         formattedPath = pathConvSlash + '/' ##...make sure to add a forward slash at the end.
         return formattedPath
+    else:
+        return pathConvSlash
